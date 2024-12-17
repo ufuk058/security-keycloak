0fe77e26ee1fd10941eada977364b1d94e316f82
ufuk058 <ufuksahinduran.uk@gmail.com>
2024-12-17 15:24:57 +0000

    initial commit

---
+++ src/main/java/com/keycloak/SecurityKeycloakApplication.java
@@ -0,0 +1,13 @@
+package com.keycloak;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SecurityKeycloakApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SecurityKeycloakApplication.class, args);
+	}
+
+}
